288643c79e692cc564fc8adecb7f069fc8b55dc3
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2022-10-19 18:16:59 -0300

    Iniciar jogo

---
+++ prototipo/src/code/level.py
@@ -0,0 +1,39 @@
+import pygame
+
+from player import Player
+from settings import *
+from tile import Tile
+
+
+class Level:
+    def __init__(self):
+        self.display_surface = pygame.display.get_surface()
+        # sprites
+        self.visible_sprites = YSortCameraGroup()
+        self.obstacle_sprites = pygame.sprite.Group()
+        # criar o mapa
+        self.create_map()
+
+    def create_map(self):
+        for row_index, row in enumerate(WORLD_MAP):
+            for col_index, col in enumerate(row):
+                x = col_index * TILESIZE
+                y = row_index * TILESIZE
+                if col == 'x':
+                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+                elif col == 'p':
+                    self.player = Player((x, y), [self.visible_sprites], self.obstacle_sprites)
+
+    def run(self):
+        self.visible_sprites.custom_draw()
+        self.visible_sprites.update()
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+
+    def custom_draw(self):
+        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
+            self.display_surface.blit(sprite.image, sprite.rect)
---
+++ prototipo/src/code/main.py
@@ -0,0 +1,34 @@
+import sys
+
+import pygame
+
+from level import Level
+from settings import *
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('Jornada à sala ALOCAR')
+        self.clock = pygame.time.Clock()
+        # level
+        self.level = Level()
+        self.run()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            self.screen.fill('black')
+            self.level.run()
+            pygame.display.update()
+            self.clock.tick(FPS)
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
