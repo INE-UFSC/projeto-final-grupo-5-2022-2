29310759c62a643445849501963e404caf2b207d
MarcosLacava <108195797+MarcosLacava@users.noreply.github.com>
2022-10-21 11:05:11 -0300

    Update tile.py

--- prototipo/src/code/tile.py
+++ prototipo/src/code/tile.py
@@ -1,10 +1,11 @@
 import pygame
+from utils import load_sprite
 
 
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load('../graphics/test/rock.png').convert_alpha()
+        self.image = load_sprite('/test/rock.png')
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -16)
         self.smaller_hitbox = self.rect.inflate(-16, -16)
