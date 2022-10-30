864aaa82caecc026be8384aeb88d5bac89ba72df
Fabricio Duarte <fabricio.duarte.jr@gmail.com>
2022-10-30 11:28:38 -0300

    Criar partículas de sangue

--- prototipo/code/particles.py
+++ prototipo/code/particles.py
@@ -20,17 +20,15 @@ class Particle(pygame.sprite.Sprite, ABC):
     def __init__(self, groups):
         super().__init__(groups)
         self.display_surface = pygame.display.get_surface()
+        self.inner_timer = 0  # um timer interno é mais suave que utilizar os ticks do pygame
 
     @abstractmethod
     def animate(self):
         pass
 
     def update(self):
+        self.inner_timer += 1
         self.animate()
-        # desenhar no update() as partículas de luz faz com que elas sejam desenhandas
-        # na frente de todos os outros sprites
-        if self.sprite_type == 'light':
-            self.display_surface.blit(self.image, self.rect)
 
 
 class FireSource(ParticleSource):
@@ -61,8 +59,7 @@ class FireParticle(Particle):
         self.color_index = 0
 
     def animate(self):
-        current_time = pygame.time.get_ticks()
-        if current_time % 4 == 0:
+        if self.inner_timer % 4 == 0:
             # diminuir e mover a partícula
             self.size = self.size - random.randint(1, 3)
             self.rect.centery -= random.randint(1, 4)
@@ -120,7 +117,6 @@ class LightParticle(Particle):
         self.diff = 0
         self.shrink_mag = shrink_mag
         self.grow_mag = grow_mag
-        self.inner_timer = 0  # um timer interno é mais suave que utilizar os ticks do pygame
 
     def animate(self):
         if self.inner_timer % 10 == 0:
@@ -136,6 +132,65 @@ class LightParticle(Particle):
         self.image.set_alpha(50)
         self.image.blit(circle, (0, 0), special_flags=pygame.BLEND_RGBA_MULT)
 
+
+class BloodSource(ParticleSource):
+    def __init__(self, pos, groups, obstacle_sprites, direction, blood_speed=32, min_particles=8, max_particles=12):
+        super().__init__(pos, groups)
+        self.obstacle_sprites = obstacle_sprites
+        self.pos = pos
+        self.direction = direction
+        self.offset = 25
+        self.blood_speed = blood_speed
+        self.min_particles = min_particles
+        self.max_particles = max_particles
+
     def update(self):
-        self.inner_timer += 1
-        super().update()
+        for i in range(random.randint(self.min_particles, self.max_particles)):
+            direction = pygame.math.Vector2(self.direction.x, self.direction.y)
+            direction.x += random.randint(-self.offset, self.offset) / 100
+            direction.y += random.randint(-self.offset, self.offset) / 100
+            direction.normalize()
+            BloodParticle(self.pos, self.groups(), self.obstacle_sprites, direction, speed=self.blood_speed)
+        self.kill()
+
+
+class BloodParticle(Particle):
+    def __init__(self, pos, groups, obstacle_sprites, direction, color='#e43b44', speed=32):
+        super().__init__(groups)
+        self.sprite_type = 'on_ground'
+        self.obstacle_sprites = obstacle_sprites
+
+        self.mask_circle_32 = load_sprite('/mask/circle_32.png')
+        self.image = self.mask_circle_32.copy()
+        self.image.set_alpha(0)  # para não desenhar a partícula ainda não configurada
+        self.rect = self.image.get_rect(center=pos)
+
+        self.direction = direction
+        self.size = 32 * random.randint(25, 150) / 100
+        self.color = color
+        self.speed = speed * random.randint(100, 150) / 100
+
+    def animate(self):
+        # diminuir e mover a partícula
+        if self.inner_timer % 2 == 0:
+            if self.inner_timer >= 5 and self.speed > 0:
+                self.size = max(1, self.size - random.randint(1, 2))
+                self.speed = max(0, self.speed - random.randint(2, 12))
+            else:
+                self.speed = max(0, self.speed - random.randint(4, 16))
+
+            self.rect.center += self.direction * self.speed
+
+        # calcular próximo alpha
+        new_alpha = self.image.get_alpha() - 1 if self.inner_timer > 100 else 255
+
+        # destruição
+        if self.size <= 4 or new_alpha == 0:
+            self.kill()
+
+        self.image = pygame.transform.scale(self.mask_circle_32, (self.size, self.size))
+        self.image.set_alpha(new_alpha)
+        circle = pygame.Surface(self.image.get_size())
+        circle.fill(self.color)
+        self.rect = circle.get_rect(center=self.rect.center)
+        self.image.blit(circle, (0, 0), special_flags=pygame.BLEND_RGBA_MULT)
