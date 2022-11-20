97929d93f0a7b92b8c656d0faf3f6a4c16d9a15b
Ricardo H <106935635+RicardoHmedeiros@users.noreply.github.com>
2022-11-20 17:22:20 -0300

    Update upgrade.py

--- prototipo/code/upgrade.py
+++ prototipo/code/upgrade.py
@@ -1,62 +1,66 @@
-import json
-import random
+from abc import abstractmethod, ABC
 
-from code.settings import GAME_PATH
+from code.utils import load_sprite
 
 
-class WaveManager:
-    def __init__(self, room_name):
-        with open(f'{GAME_PATH}/rooms/{room_name}.json', 'r', encoding='UTF-8') as file:
-            self.__wave_data = json.load(file)[0]
+class Upgrade(ABC):
+    def __init__(self, name, description, icon):
+        self.__name = name
+        self.__description = description
+        self.__icon = load_sprite(icon)
 
-        self.__timer = 0
-        self.__tick_index = 0
-        self.__max_time = 5400  # 1 minuto e 30 segundos
-
-        self.__spawn_positions = ((200, 200), (500, 500))
-
-    def update(self, spawn_enemy):
-        if self.__timer >= self.__max_time:
-            return
-
-        self.__timer += 1
-
-        # lógica de avançar tick
-        while len(self.__wave_data['ticks']) - 1 > self.__tick_index and \
-                self.__timer >= self.__wave_data['ticks'][self.__tick_index + 1]['timer']:
-            self.__tick_index += 1
-
-        tick = self.__wave_data['ticks'][self.__tick_index]['every']
-        # spawn de inimigos
-        if self.__timer % tick == 0:
-            available_spawns = list(
-                filter(lambda spawn: spawn['after'] <= self.__timer <= spawn['before'],
-                       self.__wave_data['spawns']))
-            chosen_spawn = random.choice(available_spawns)
-            enemies = chosen_spawn['enemies'].split('/')
-            locations = chosen_spawn['locations'].split('/')
-            for enemy, location in zip(enemies, locations):
-                # as localizações possíveis atualmente são:
-                # random - escolhe uma posiçaõ aleatoriamente
-                # previous - spawna na mesma posição do anterior
-                if location == 'random':
-                    spawn_position = random.choice(self.__spawn_positions)
-                spawn_enemy(enemy, spawn_position)
+    @abstractmethod
+    def apply(self, player):
+        pass
 
     @property
-    def timer(self):
-        return self.__timer
+    def name(self):
+        return self.__name
 
     @property
-    def tick_index(self):
-        return self.__tick_index
+    def description(self):
+        return self.__description
 
     @property
-    def max_time(self):
-        return self.__max_time
-
-    @property
-    def spawn_positions(self):
-        return self.__spawn_positions
+    def icon(self):
+        return self.__icon
 
 
+class HealthUpgrade(Upgrade):
+    def __init__(self):
+        super().__init__('Health Upgrade',
+                         'Aumenta a quantidade de vidas em 1',
+                         '/icon/health_upgrade.png')
+
+    def apply(self, player):
+        player.give_health(1)
+
+
+class DamageUpgrade(Upgrade):
+    def __init__(self):
+        super().__init__('Damage Upgrade',
+                         'Aumenta o dano da bola de fogo em 20%',
+                         '/icon/damage_upgrade.png')
+
+    def apply(self, player):
+        player.attacks[0].damage += player.attacks[0].base_damage * 0.20
+
+
+class FireRateUpgrade(Upgrade):
+    def __init__(self):
+        super().__init__('Fire Rate Upgrade',
+                         'Reduz 5% do cooldown da bola de fogo',
+                         '/icon/fire_rate_upgrade.png')
+
+    def apply(self, player):
+        player.attacks[0].cooldown *= 0.95
+
+
+class SpeedUpgrade(Upgrade):
+    def __init__(self):
+        super().__init__('Speed Upgrade',
+                         'Aumenta a velocidade em 5%',
+                         '/icon/speed_upgrade.png')
+
+    def apply(self, player):
+        player.move_speed = player.move_speed + player.base_speed * 0.05
