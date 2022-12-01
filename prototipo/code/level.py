6a8a9e18438be16120d7024fd099460a51d9e8a4
MarcosLacava <marcoslaurean@gmail.com>
2022-12-01 20:45:07 -0300

    Criado o fade in na troca de salas

--- prototipo/code/level.py
+++ prototipo/code/level.py
@@ -1,6 +1,7 @@
 from code.room import Room
-from code.settings import WIDTH
-
+from code.settings import *
+from code.fade import Fade
+from code.group_manager import GroupManager
 
 class Level:
     def __init__(self):
@@ -8,23 +9,37 @@ class Level:
         self.__rooms = ('1', '2')
         self.__current_room_index = 0
         self.__room = Room(self.__rooms[self.__current_room_index])
+        self.__changing_room = False
 
     def next_room(self):
-        if self.__current_room_index < len(self.__rooms) - 1:
-            self.__current_room_index += 1
-            self.__room.change_to(self.__rooms[self.__current_room_index])
-        else:
-            self.end_level() 
-
+            # troca de sala
+            if self.__current_room_index < len(self.__rooms) - 1:
+                self.__current_room_index += 1
+                self.__room.change_to(self.__rooms[self.__current_room_index])
+                # TODO: arrumar o fade out
+                self.__Fade = Fade(self.__room.group_manager.visible_sprites, fade_step=3, fade_in=True)
+            else:
+                self.end_level()
+                
     def end_level(self):
-        # TODO: implementar o endlevel
-        print("Level end")
+        # TODO: implementar end_level
+        print('end level')
 
     def toggle_menu(self):
         self.__room.toggle_menu()
 
     def run(self):
         self.__room.run()
+
+        if hasattr(self, '_Level__Fade') and self.__Fade.step():
+            if self.__changing_room:
+                self.__changing_room = False
+                self.next_room()
+            self.__Fade.kill()
+        
         # TODO: conferir se a wave já terminou
-        if self.__room.player.rect.topleft[0] > WIDTH - 192:  # mudar
-            self.next_room()
+        if self.__room.player.rect.topleft[0] > WIDTH - 192 and not self.__changing_room:  # mudar
+            self.__Fade = Fade(self.__room.group_manager.visible_sprites, fade_step=3, fade_in=False)
+            self.__changing_room = True
+
+
