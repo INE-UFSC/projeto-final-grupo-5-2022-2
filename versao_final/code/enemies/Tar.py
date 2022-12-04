0749e2216f4f9184a64baf6e44906a49650cd5e4
MarcosLacava <marcoslaurean@gmail.com>
2022-12-04 17:48:16 -0300

    Arrumado a velocidade do tar

--- versao_final/code/enemies/Tar.py
+++ versao_final/code/enemies/Tar.py
@@ -3,5 +3,5 @@ from code.enemies.Enemy import Enemy
 
 class Tar(Enemy):
     def __init__(self, pos):
-        super().__init__('tar', pos, health=3, speed=1, collision_damage=1, exp=1,
+        super().__init__('tar', pos, health=3, speed=1.5, collision_damage=1, exp=1,
                          attack_cooldown=85)
